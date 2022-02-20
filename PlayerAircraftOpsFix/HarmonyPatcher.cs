abded0821c7dfe906b814d6b583155e03f40d28e
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2022-02-20 19:27:39 +0000

    Initial Commit

---
+++ PlayerAircraftOpsFix/HarmonyPatcher.cs
@@ -0,0 +1,43 @@
+﻿using BalsaCore;
+using HarmonyLib;
+using IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace CloverTech
+{
+    class HarmonyContainer
+    {
+        public static Harmony harmony = new Harmony("Balsa.CloverTech.PlayerAircraftOpsFix");
+
+        public static void DoPatches()
+        {
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Scenarios.Modules.PlayerAircraftOps), "OnVehicleLogEvent")]
+        class PlayerAircraftOpsFixPatch
+        {
+            static bool Prefix()
+            {
+                Vehicle localPlayerVehicle = GameLogic.LocalPlayerVehicle;
+                if ( localPlayerVehicle == null )
+                {
+#if DEBUG
+                    FileLog.Log("NullRef in PlayerAircraftOps");
+#endif
+                    return false;
+                }
+                return true;
+            }
+        }
+
+    }
+}
---
+++ PlayerAircraftOpsFix/Loader.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using BalsaCore;
+using IO;
+using UI;
+using UI.MMX.Data;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.IO;
+
+namespace CloverTech
+{
+    public class PlayerAircraftOpsFix : MonoBehaviour
+    {
+        public void Start()
+        {
+            DontDestroyOnLoad(this);
+            LogW("Test");
+#if DEBUG
+            Debug.LogError("Init");
+#endif
+            HarmonyContainer.DoPatches();
+        }
+
+        public void LogI(string message)
+        {
+            Debug.Log($"[PlayerAircraftOpsFix] {message}");
+        }
+        public void LogW(string message)
+        {
+            Debug.LogWarning($"[PlayerAircraftOpsFix] {message}");
+        }
+        public void LogE(string message)
+        {
+            Debug.LogError($"[PlayerAircraftOpsFix] {message}");
+        }
+    }
+    
+
+    [BalsaAddon]
+    public class Loader
+    {
+        private static GameObject go = null;
+        private static bool initialised = false;
+
+        [BalsaAddonInit]
+        public static void BalsaInit()
+        {
+            if (!initialised)
+            {
+                Debug.Log("[PlayerAircraftOpsFix] Creating GameObject");
+                go = new GameObject("CloverTech::PlayerAircraftOpsFix");
+                go.AddComponent<PlayerAircraftOpsFix>();
+                initialised = true;
+            }
+        }
+
+        [BalsaAddonInit(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaInitFlight()
+        {
+
+        }
+
+        [BalsaAddonFinalize(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaFinalizeFlight()
+        {
+
+        }
+        //Game exit
+        [BalsaAddonFinalize]
+        public static void BalsaFinalize()
+        {
+            go.DestroyGameObject();
+        }
+
+    }
+}
